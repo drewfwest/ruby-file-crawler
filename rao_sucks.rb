f1b5d47d7f56710612340d2e5e0baede9942fc70
Drew West <drewfwest@gmail.com>
2014-03-26 23:08:51 -0700

    Add Pseudocode for file crawler   *Also created rao_sucks method which successfully appends to a test file.
    TODO
      *Now figure out how to iterate thorough all files in a directory.
      *How do I see all files in a directory?

---
+++ rao_sucks.rb
@@ -0,0 +1,27 @@
+# PSEUDOCODE
+# Input Directory
+# Output: "#Rao sucks." Comment added at the top of each .rb file in specified directory.
+# STEPS:
+# 1. Require any required libraries or gems.
+# 2. Take directory as input and be able to pass as an argument to methods.
+# 3. Open each file in directory.
+# 4. Append the comment "#Rao sucks." to the top of each file.
+# 5. Save file.
+# 6. Close file.
+# 7. Check to make sure change is permanent.
+
+# require 'filelutils'
+
+# def iterate_files(directory)
+#   directory.each do {|file| rao_sucks(file) }
+# end
+
+
+def rao_sucks(file)
+  # File.open(file) do
+  File.open(file, mode='w') { |file| file.write('#Rao sucks.') }
+end
+
+rao_sucks('test.rb')
+
+# iterate_files('./')
